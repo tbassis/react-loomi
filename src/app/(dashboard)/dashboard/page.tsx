b287c175499ad925d426ee3e4f4f72db453953a0
Tulio <tulioassis.18@gmail.com>
2025-12-16 10:58:14 -0300

    react-loomi/3 - Initial cards implementation

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -10,9 +10,26 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="min-h-full pt-[56px]">
-      <h1 className="text-xl font-bold">Dashboard</h1>
-      <p>Usuário autenticado</p>
+    <div className="min-h-full flex flex-col items-center justify-center pt-[56px]">
+      <div className="flex flex-col items-center gap-10">
+        <div className="flex gap-10">
+          <div className="bg-on-background p-6 pt-9 rounded-[24px]">
+            <div className="flex justify-between mb-6">
+              <h2 className="font-bold text-xl">Evolução KPI</h2>
+            </div>
+          </div>
+          <div className="bg-on-background p-6 pt-9 rounded-[24px]">
+            <div className="flex justify-between mb-6">
+              <h2 className="font-bold text-xl">Taxa de conversão</h2>
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-on-background p-6 pt-9 rounded-[24px]">
+          <div className="flex justify-between mb-6">
+            <h2 className="font-bold text-xl">Mapa de clientes por região</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
