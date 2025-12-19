68ab3ace34065da90b1768ef464bdc01ca62cd9a
Tulio <tulioassis.18@gmail.com>
2025-12-19 19:39:46 -0300

    react-loomi/4 - First ticket commit

--- src/app/(dashboard)/ticket/page.tsx
+++ src/app/(dashboard)/ticket/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default async function DashboardPage() {
   const cookieStore = await cookies();
@@ -9,5 +10,45 @@ export default async function DashboardPage() {
     redirect("/login");
   }
 
-  return <div>Pagina de tickets</div>;
+  return (
+    <div className="min-h-full flex flex-col items-center justify-center pt-[56px]">
+      <div className="flex flex-col items-center gap-10 w-4/5 max-w-[1370px] mx-auto">
+        <div className="flex gap-6 ">
+          <div className="flex flex-col gap-7 p-5 bg-[#FFFFFF0D] rounded-2xl">
+            <p className="text-sm font-normal">Tickets Abertos</p>
+            <div className="flex justify-between">
+              <span className="text-[32px] font-bold">15</span>
+              <Image src="images/open-ticket.svg" alt="Open Ticket" width={32} height={32} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-7 p-5 bg-[#FFFFFF0D] rounded-2xl">
+            <p className="text-sm font-normal">Em andamento</p>
+            <div className="flex justify-between">
+              <span className="text-[32px] font-bold">15</span>
+              <Image
+                src="images/progress-ticket.svg"
+                alt="Progress Ticket"
+                width={32}
+                height={32}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-7 p-5 bg-[#FFFFFF0D] rounded-2xl">
+            <p className="text-sm font-normal">Resolveidos hoje</p>
+            <div className="flex justify-between">
+              <span className="text-[32px] font-bold">15</span>
+              <Image src="images/done-ticket.svg" alt="Done Ticket" width={32} height={32} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-7 p-5 bg-[#FFFFFF0D] rounded-2xl">
+            <p className="text-sm font-normal">Tempo médio</p>
+            <div className="flex justify-between">
+              <span className="text-[32px] font-bold">15</span>
+              <Image src="images/average-ticket.svg" alt="Average Ticket" width={32} height={32} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
